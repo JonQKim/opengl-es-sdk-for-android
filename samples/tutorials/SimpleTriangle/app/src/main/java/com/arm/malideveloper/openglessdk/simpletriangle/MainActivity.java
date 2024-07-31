9bda525d7423b885e7c45062a2c3486376cdf75e
JonQKim <jonqkim@gmail.com>
2024-08-01 00:42:07 +0900

    Remove unnecessary lines in onCreate of MainActivity (Crash Fixed)

--- samples/tutorials/SimpleTriangle/app/src/main/java/com/arm/malideveloper/openglessdk/simpletriangle/MainActivity.java
+++ samples/tutorials/SimpleTriangle/app/src/main/java/com/arm/malideveloper/openglessdk/simpletriangle/MainActivity.java
@@ -17,21 +17,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-//        setContentView(R.layout.activity_main);
-        Log.i(LOGTAG, "Creating New Tutorial View");
-        graphicsView = new TutorialView(getApplication());
-        setContentView(graphicsView);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-		
+        Log.i(LOGTAG, "Creating New Tutorial View");
+        graphicsView = new TutorialView(this);
+
+        setContentView(graphicsView);
     }
 	
-	    @Override protected void onPause()
+    @Override protected void onPause()
     {
         super.onPause();
         graphicsView.onPause();
