a32a27fdee1218be2b991b23b0bb44008a3222f0
JonQKim <jonqkim@gmail.com>
2024-08-17 03:47:58 +0900

    Update Lighting

--- samples/tutorials/Lighting/app/src/main/cpp/lighting.cpp
+++ samples/tutorials/Lighting/app/src/main/cpp/lighting.cpp
@@ -40,13 +40,45 @@
 static const char  glVertexShader[] =
         "attribute vec4 vertexPosition;\n"
         "attribute vec3 vertexColour;\n"
+        "attribute vec3 vertexNormal;\n"
         "varying vec3 fragColour;\n"
         "uniform mat4 projection;\n"
         "uniform mat4 modelView;\n"
         "void main()\n"
         "{\n"
+             /* [Setup scene vectors.] */
+        "    vec3 transformedVertexNormal = normalize((modelView * vec4(vertexNormal, 0.0)).xyz);"
+        "    vec3 inverseLightDirection = normalize(vec3(0.0, 1.0, 1.0));\n"
+		"    fragColour = vec3(0.0);\n"
+		     /* [Setup scene vectors.] */
+		"\n"
+		     /* [Calculate the diffuse component.] */
+		"    vec3 diffuseLightIntensity = vec3(1.0, 1.0, 1.0);\n"
+		"    vec3 vertexDiffuseReflectionConstant = vertexColour;\n"
+		"    float normalDotLight = max(0.0, dot(transformedVertexNormal, inverseLightDirection));\n"
+		"    fragColour += normalDotLight * vertexDiffuseReflectionConstant * diffuseLightIntensity;\n"
+		     /* [Calculate the diffuse component.] */
+		"\n"
+             /* [Calculate the ambient component.] */
+		"    vec3 ambientLightIntensity = vec3(0.1, 0.1, 0.1);\n"
+		"    vec3 vertexAmbientReflectionConstant = vertexColour;\n"
+		"    fragColour += vertexAmbientReflectionConstant * ambientLightIntensity;\n"
+		     /* [Caluclate the ambient component.] */
+        "\n"
+		     /* [Calculate the specular component.] */
+		"    vec3 inverseEyeDirection = normalize(vec3(0.0, 0.0, 1.0));\n"
+		"    vec3 specularLightIntensity = vec3(1.0, 1.0, 1.0);\n"
+		"    vec3 vertexSpecularReflectionConstant = vec3(1.0, 1.0, 1.0);\n"
+		"    float shininess = 2.0;\n"
+		"    vec3 lightReflectionDirection = reflect(vec3(0) - inverseLightDirection, transformedVertexNormal);\n"
+		"    float normalDotReflection = max(0.0, dot(inverseEyeDirection, lightReflectionDirection));\n"
+		"    fragColour += pow(normalDotReflection, shininess) * vertexSpecularReflectionConstant * specularLightIntensity;\n"
+		     /* [Calculate the specular component.] */
+		"\n"
+		"    /* Make sure the fragment colour is between 0 and 1. */"
+		"    clamp(fragColour, 0.0, 1.0);\n"
+		"\n"
         "    gl_Position = projection * modelView * vertexPosition;\n"
-        "    fragColour = vertexColour;\n"
         "}\n";
 /* [vertexShader] */
 
@@ -141,9 +173,10 @@ GLuint createProgram(const char* vertexSource, const char * fragmentSource)
     return program;
 }
 
-GLuint simpleCubeProgram;
+GLuint lightingProgram;
 GLuint vertexLocation;
 GLuint vertexColourLocation;
+GLuint vertexNormalLocation;
 GLuint projectionLocation;
 GLuint modelViewLocation;
 
@@ -154,18 +187,19 @@ float angle = 0;
 /* [setupGraphics] */
 bool setupGraphics(int width, int height)
 {
-    simpleCubeProgram = createProgram(glVertexShader, glFragmentShader);
+    lightingProgram = createProgram(glVertexShader, glFragmentShader);
 
-    if (simpleCubeProgram == 0)
+    if (lightingProgram == 0)
     {
         LOGE ("Could not create program");
         return false;
     }
 
-    vertexLocation = glGetAttribLocation(simpleCubeProgram, "vertexPosition");
-    vertexColourLocation = glGetAttribLocation(simpleCubeProgram, "vertexColour");
-    projectionLocation = glGetUniformLocation(simpleCubeProgram, "projection");
-    modelViewLocation = glGetUniformLocation(simpleCubeProgram, "modelView");
+    vertexLocation = glGetAttribLocation(lightingProgram, "vertexPosition");
+    vertexColourLocation = glGetAttribLocation(lightingProgram, "vertexColour");
+    vertexNormalLocation = glGetAttribLocation(lightingProgram, "vertexNormal");
+    projectionLocation = glGetUniformLocation(lightingProgram, "projection");
+    modelViewLocation = glGetUniformLocation(lightingProgram, "modelView");
 
     /* Setup the perspective */
     matrixPerspective(projectionMatrix, 45, (float)width / (float)height, 0.1f, 100);
@@ -177,63 +211,115 @@ bool setupGraphics(int width, int height)
 }
 /* [setupGraphics] */
 
-/* [cubeVertices] */
-GLfloat cubeVertices[] = {-1.0f,  1.0f, -1.0f, /* Back. */
-                           1.0f,  1.0f, -1.0f,
-                          -1.0f, -1.0f, -1.0f,
-                           1.0f, -1.0f, -1.0f,
-                          -1.0f,  1.0f,  1.0f, /* Front. */
-                           1.0f,  1.0f,  1.0f,
-                          -1.0f, -1.0f,  1.0f,
-                           1.0f, -1.0f,  1.0f,
-                          -1.0f,  1.0f, -1.0f, /* Left. */
-                          -1.0f, -1.0f, -1.0f,
-                          -1.0f, -1.0f,  1.0f,
-                          -1.0f,  1.0f,  1.0f,
-                           1.0f,  1.0f, -1.0f, /* Right. */
-                           1.0f, -1.0f, -1.0f,
-                           1.0f, -1.0f,  1.0f,
-                           1.0f,  1.0f,  1.0f,
-                          -1.0f, -1.0f, -1.0f, /* Top. */
-                          -1.0f, -1.0f,  1.0f,
-                           1.0f, -1.0f,  1.0f,
-                           1.0f, -1.0f, -1.0f,
-                          -1.0f,  1.0f, -1.0f, /* Bottom. */
-                          -1.0f,  1.0f,  1.0f,
-                           1.0f,  1.0f,  1.0f,
-                           1.0f,  1.0f, -1.0f
-                         };
-/* [cubeVertices] */
-/* [colourComponents] */
-GLfloat colour[] = {1.0f, 0.0f, 0.0f,
+/* [vertices] */
+GLfloat vertices[] = { 1.0f,  1.0f, -1.0f, /* Back. */
+                        -1.0f,  1.0f, -1.0f,
+                        1.0f, -1.0f, -1.0f,
+                        -1.0f, -1.0f, -1.0f,
+                        0.0f,  0.0f, -2.0f,
+                        -1.0f,  1.0f,  1.0f, /* Front. */
+                        1.0f,  1.0f,  1.0f,
+                        -1.0f, -1.0f,  1.0f,
+                        1.0f, -1.0f,  1.0f,
+                        0.0f,  0.0f,  2.0f,
+                        -1.0f,  1.0f, -1.0f, /* Left. */
+                        -1.0f,  1.0f,  1.0f,
+                        -1.0f, -1.0f, -1.0f,
+                        -1.0f, -1.0f,  1.0f,
+                        -2.0f,  0.0f,  0.0f,
+                        1.0f,  1.0f,  1.0f, /* Right. */
+                        1.0f,  1.0f, -1.0f,
+                        1.0f, -1.0f,  1.0f,
+                        1.0f, -1.0f, -1.0f,
+                        2.0f,  0.0f,  0.0f,
+                        -1.0f, -1.0f,  1.0f, /* Bottom. */
+                        1.0f, -1.0f,  1.0f,
+                        -1.0f, -1.0f, -1.0f,
+                        1.0f, -1.0f, -1.0f,
+                        0.0f, -2.0f,  0.0f,
+                        -1.0f,  1.0f, -1.0f, /* Top. */
+                        1.0f,  1.0f, -1.0f,
+                        -1.0f,  1.0f,  1.0f,
+                        1.0f,  1.0f,  1.0f,
+                        0.0f,  2.0f,  0.0f
+};
+/* [vertices] */
+/* [colours] */
+GLfloat colour[] = {1.0f, 0.0f, 0.0f, /* Back. */
                     1.0f, 0.0f, 0.0f,
                     1.0f, 0.0f, 0.0f,
                     1.0f, 0.0f, 0.0f,
+                    1.0f, 0.0f, 0.0f,
+                    0.0f, 1.0f, 0.0f, /* Front. */
                     0.0f, 1.0f, 0.0f,
                     0.0f, 1.0f, 0.0f,
                     0.0f, 1.0f, 0.0f,
                     0.0f, 1.0f, 0.0f,
+                    0.0f, 0.0f, 1.0f, /* Left. */
                     0.0f, 0.0f, 1.0f,
                     0.0f, 0.0f, 1.0f,
                     0.0f, 0.0f, 1.0f,
                     0.0f, 0.0f, 1.0f,
+                    1.0f, 1.0f, 0.0f, /* Right. */
                     1.0f, 1.0f, 0.0f,
                     1.0f, 1.0f, 0.0f,
                     1.0f, 1.0f, 0.0f,
                     1.0f, 1.0f, 0.0f,
+                    0.0f, 1.0f, 1.0f, /* Bottom. */
                     0.0f, 1.0f, 1.0f,
                     0.0f, 1.0f, 1.0f,
                     0.0f, 1.0f, 1.0f,
                     0.0f, 1.0f, 1.0f,
+                    1.0f, 0.0f, 1.0f, /* Top. */
                     1.0f, 0.0f, 1.0f,
                     1.0f, 0.0f, 1.0f,
                     1.0f, 0.0f, 1.0f,
                     1.0f, 0.0f, 1.0f
-                   };
-/* [colourComponents] */
+};
+/* [colours] */
 
-/* [indices] */
-GLushort indices[] = {0, 2, 3, 0, 1, 3, 4, 6, 7, 4, 5, 7, 8, 9, 10, 11, 8, 10, 12, 13, 14, 15, 12, 14, 16, 17, 18, 16, 19, 18, 20, 21, 22, 20, 23, 22};
+
+GLfloat normals[] = { 1.0f,  1.0f, -1.0f, /* Back. */
+                      -1.0f,  1.0f, -1.0f,
+                      1.0f, -1.0f, -1.0f,
+                      -1.0f, -1.0f, -1.0f,
+                      0.0f,  0.0f, -1.0f,
+                      -1.0f,  1.0f,  1.0f, /* Front. */
+                      1.0f,  1.0f,  1.0f,
+                      -1.0f, -1.0f,  1.0f,
+                      1.0f, -1.0f,  1.0f,
+                      0.0f,  0.0f,  1.0f,
+                      -1.0f,  1.0f, -1.0f, /* Left. */
+                      -1.0f,  1.0f,  1.0f,
+                      -1.0f, -1.0f, -1.0f,
+                      -1.0f, -1.0f,  1.0f,
+                      -1.0f,  0.0f,  0.0f,
+                      1.0f,  1.0f,  1.0f, /* Right. */
+                      1.0f,  1.0f, -1.0f,
+                      1.0f, -1.0f,  1.0f,
+                      1.0f, -1.0f, -1.0f,
+                      1.0f,  0.0f,  0.0f,
+                      -1.0f, -1.0f,  1.0f, /* Bottom. */
+                      1.0f, -1.0f,  1.0f,
+                      -1.0f, -1.0f, -1.0f,
+                      1.0f, -1.0f, -1.0f,
+                      0.0f, -1.0f,  0.0f,
+                      -1.0f,  1.0f, -1.0f, /* Top. */
+                      1.0f,  1.0f, -1.0f,
+                      -1.0f,  1.0f,  1.0f,
+                      1.0f,  1.0f,  1.0f,
+                      0.0f,  1.0f,  0.0f
+                     };
+/* [Normals] */
+
+/* [Indices] */
+GLushort indices[] = {0,  2,  4,  0,  4,  1,  1,  4,  3,  2,  3,  4,  /* Back. */
+                      5,  7,  9,  5,  9,  6,  6,  9,  8,  7,  8,  9,  /* Front. */
+                      10, 12, 14, 10, 14, 11, 11, 14, 13, 12, 13, 14, /* Left. */
+                      15, 17, 19, 15, 19, 16, 16, 19, 18, 17, 18, 19, /* Right. */
+                      20, 22, 24, 20, 24, 21, 21, 24, 23, 22, 23, 24, /* Bottom. */
+                      25, 27, 29, 25, 29, 26, 26, 29, 28, 27, 28, 29  /* Top. */
+};
 /* [indices] */
 
 /* [renderFrame] */
@@ -249,16 +335,18 @@ void renderFrame()
 
     matrixTranslate(modelViewMatrix, 0.0f, 0.0f, -10.0f);
 
-    glUseProgram(simpleCubeProgram);
-    glVertexAttribPointer(vertexLocation, 3, GL_FLOAT, GL_FALSE, 0, cubeVertices);
+    glUseProgram(lightingProgram);
+    glVertexAttribPointer(vertexLocation, 3, GL_FLOAT, GL_FALSE, 0, vertices);
     glEnableVertexAttribArray(vertexLocation);
     glVertexAttribPointer(vertexColourLocation, 3, GL_FLOAT, GL_FALSE, 0, colour);
     glEnableVertexAttribArray(vertexColourLocation);
+    glVertexAttribPointer(vertexNormalLocation, 3, GL_FLOAT, GL_FALSE, 0, normals);
+    glEnableVertexAttribArray(vertexNormalLocation);
 
     glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, projectionMatrix);
     glUniformMatrix4fv(modelViewLocation, 1, GL_FALSE, modelViewMatrix);
 
-    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, indices);
+    glDrawElements(GL_TRIANGLES, 72, GL_UNSIGNED_SHORT, indices);
 
     angle += 1;
     if (angle > 360)
