3add10dbc9e2fb5ff05b3405a585a552abf1c5b8
JonQKim <jonqkim@gmail.com>
2025-04-10 00:15:55 +0900

    Clean code - FirstNative

--- samples/tutorials/FirstNative/app/src/main/cpp/firstnative.cpp
+++ samples/tutorials/FirstNative/app/src/main/cpp/firstnative.cpp
@@ -21,12 +21,11 @@
 /* [Includes] */
 #include <jni.h>
 #include <android/log.h>
-/* [Includes] */
 
 #define LOG_TAG "libNative"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-
+/* [Includes] */
 /* [Function definitions] */
 extern "C"
 {
--- samples/tutorials/FirstNative/app/src/main/java/com/arm/malideveloper/openglessdk/firstnative/MainActivity.java
+++ samples/tutorials/FirstNative/app/src/main/java/com/arm/malideveloper/openglessdk/firstnative/MainActivity.java
@@ -1,28 +1,57 @@
+/* Copyright (c) 2013-2017, ARM Limited and Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge,
+ * to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/* [Imports] */
 package com.arm.malideveloper.openglessdk.firstnative;
 
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+/* [Imports] */
 
+/* [Class definition] */
 public class MainActivity extends AppCompatActivity {
     private static final String LOGTAG = "MainActivity";
+    /* [Class definition] */	
 
+    /* [onCreate] */	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(LOGTAG, "On Create Method Calling Native Library");
         NativeLibrary.init();
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+	/* [onCreate] */	
+	
+	/* [onPause and onResume] */
+	@Override
+	protected void onPause()
+	{
+		super.onPause();
+	}
+	
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+	}
+    /*  [onPause and onResume] */
 }
